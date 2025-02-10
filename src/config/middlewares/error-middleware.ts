9fc269cc64b6836c89529a4d67bbeb944a09d0b5
Julio Tavares <juliotavares197@gmail.com>
2025-02-09 21:11:05 -0300

    chore: jwt validation error

--- src/config/middlewares/error-middleware.ts
+++ src/config/middlewares/error-middleware.ts
@@ -1,4 +1,9 @@
-import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify'
+import {
+    FastifyError,
+    FastifyInstance,
+    FastifyReply,
+    FastifyRequest,
+} from 'fastify'
 import { ZodError } from 'zod'
 import { BaseException } from '../../shared/errors/base-exception'
 
@@ -21,6 +26,22 @@ export function registerErrorMiddleware(app: FastifyInstance) {
                     errors: formattedErrors,
                 })
             }
+
+            if ((error as FastifyError).code === 'FAST_JWT_EXPIRED') {
+                return reply.status(401).send({
+                    status: 'error',
+                    message: 'Token expired. Please log in again.',
+                })
+            }
+
+            if ((error as FastifyError).code?.startsWith('FAST_JWT')) {
+                return reply.status(401).send({
+                    status: 'error',
+                    message: 'Invalid or missing token',
+                })
+            }
+
+            console.log(`Error instance of: ${error.constructor.name}`)
 
             console.error('Unexpected error:', error)
             return reply.status(500).send({ message: 'Internal server error' })
