c4076e243fae7b70339bbd98df6ca2207178a417
Julio Tavares <juliotavares197@gmail.com>
2025-02-20 23:02:45 -0300

    chore: linting code

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,10 @@
-import "dotenv/config";
-import { defineConfig } from "drizzle-kit";
+import 'dotenv/config'
+import { defineConfig } from 'drizzle-kit'
 
 export default defineConfig({
-  schema: "./src//infrastructure/db/drizzle/schemas",
-  dialect: "postgresql",
-  dbCredentials: {
-    url: process.env.DATABASE_URL!,
-  },
-});
+    schema: './src//infrastructure/db/drizzle/schemas',
+    dialect: 'postgresql',
+    dbCredentials: {
+        url: process.env.DATABASE_URL!,
+    },
+})
--- src/infrastructure/db/drizzle/schemas/users.ts
+++ src/infrastructure/db/drizzle/schemas/users.ts
@@ -1,9 +1,9 @@
-import { pgTable, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
+import { pgTable, timestamp, uuid, varchar } from 'drizzle-orm/pg-core'
 
-export const users = pgTable("users", {
-  id: uuid("id").primaryKey().defaultRandom(),
-  name: varchar("name", { length: 255 }).notNull(),
-  email: varchar("email", { length: 255 }).unique().notNull(),
-  password: varchar("password", { length: 255 }).notNull(),
-  createdAt: timestamp("created_at").defaultNow(),
-});
+export const users = pgTable('users', {
+    id: uuid('id').primaryKey().defaultRandom(),
+    name: varchar('name', { length: 255 }).notNull(),
+    email: varchar('email', { length: 255 }).unique().notNull(),
+    password: varchar('password', { length: 255 }).notNull(),
+    createdAt: timestamp('created_at').defaultNow(),
+})
